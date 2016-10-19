95945c5cdc80863e48b292a9b7ac7e89b2ee4148
2160429 <2160429@C2-LSO-M01.IPLeiria.pt>
2016-10-19 13:41:07 +0100

    Update 19-10-2016

---
+++ src/myproject/MyProject.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject;
+
+/**
+ *
+ * @author 2160429
+ */
+public class MyProject {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String []args) {
+      System.out.println("Insira um numero");
+      byte number; 
+      ola(6);
+   }
+   public static void ola(int text){
+   String nome=" Rui";
+   if (text==4){
+   System.out.print("Olá eu sou o" +nome +"\n");   
+   }else{
+   System.out.print("Erro nª: "+text+"\n");   
+   } 
+   }
+}
